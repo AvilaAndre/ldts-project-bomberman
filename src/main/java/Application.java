d503aa02f6497d6f8e607cb1ca330e913f33f96b
André Ávila <avila_andre14@hotmail.com>
2022-01-01 22:57:47 +0000

    Application created

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,58 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFrame;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new Application().run();
+    }
+
+    private void run() {
+        try {
+            URL resource = getClass().getClassLoader().getResource("square.ttf");
+            File fontFile = new File(resource.toURI());
+            Font font =  Font.createFont(Font.TRUETYPE_FONT, fontFile);
+
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(font);
+
+            DefaultTerminalFactory factory = new DefaultTerminalFactory();
+
+            Font loadedFont = font.deriveFont(Font.PLAIN, 15);
+            AWTTerminalFontConfiguration fontConfig = AWTTerminalFontConfiguration.newInstance(loadedFont);
+            factory.setTerminalEmulatorFontConfiguration(fontConfig);
+            factory.setForceAWTOverSwing(true);
+            factory.setInitialTerminalSize(new TerminalSize(98, 52));
+
+            Terminal terminal = factory.createTerminal();
+            ((AWTTerminalFrame)terminal).addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    e.getWindow().dispose();
+                }
+            });
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+            screen.clear();
+            screen.refresh();
+            //menu
+            screen.stopScreen();
+        } catch (IOException | FontFormatException | URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 }
