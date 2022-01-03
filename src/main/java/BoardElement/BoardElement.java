e071c8324622ad26f35d9fedb16c8b311ec20272
André Ávila <avila_andre14@hotmail.com>
2022-01-03 16:27:23 +0000

    Incomplete BoardElement created.
    Board, DrawingImage, Position and ColliderBox are classes needed to complete this class.

---
+++ src/main/java/BoardElement/BoardElement.java
@@ -0,0 +1,22 @@
+package BoardElement;
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public abstract class BoardElement {
+    DrawingImage image = null;
+    ArrayAsList<ColliderBox> = new ArrayAsList<>();
+    Position position;
+    Board board = null;
+
+
+    BoardElement(String color_, Position pos_, Board gameBoard_) {
+        this.color = color_;
+        this.position = pos_;
+        this.board = gameBoard_;
+    }
+
+    public void draw(TextGraphics graphics_) {
+        image.draw(graphics_);
+    }
+
+}
