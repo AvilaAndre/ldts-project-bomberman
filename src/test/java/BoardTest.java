c46c80df8fff79aaed596fe1842a4e549cbdcc1e
André Ávila <avila_andre14@hotmail.com>
2022-01-04 01:54:41 +0000

    Started working on the class.

---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,31 @@
+import BoardComponents.Board;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BoardTest {
+    @Test
+    public void BoardCreationTest() throws IOException {
+        Board testBoard = new Board(
+                "000000000000000000" +
+                        "0  111000000111  0" +
+                        "0 01110000001110 0" +
+                        "010000000000000010" +
+                        "01  1  1111  1  10" +
+                        "01011100  00111010" +
+                        "0  1110    0111  0" +
+                        "0  1110    0111  0" +
+                        "0  1110    0111  0" +
+                        "0  1110    0111  0" +
+                        "01011100  00111010" +
+                        "01  1  1111  1  10" +
+                        "010000000000000010" +
+                        "0 01110000001110 0" +
+                        "0  111000000111  0" +
+                        "000000000000000000");
+
+    }
+}
+
