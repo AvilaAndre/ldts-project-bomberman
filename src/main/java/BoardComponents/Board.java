2b594081e66776f2ae297c03c873f7aaef6e1537
André Ávila <avila_andre14@hotmail.com>
2022-01-04 01:55:16 +0000

    Squashed commit of the following:
    commit c46c80df8fff79aaed596fe1842a4e549cbdcc1e
    Author: André Ávila <avila_andre14@hotmail.com>
    Date:   Tue Jan 4 01:54:41 2022 +0000
    
        Started working on the class.

--- src/main/java/BoardComponents/Board.java
+++ src/main/java/BoardComponents/Board.java
@@ -1,5 +1,40 @@
 package BoardComponents;
 
-public class Board {
+import Structures.Position;
+import com.googlecode.lanterna.graphics.TextGraphics;
 
+import java.util.ArrayList;
+
+public class Board {
+    TextGraphics graphics = null;
+    private ArrayList<BoardElement> blocks = new ArrayList<>();
+    //private ArrayList<Bomb> bombs = new ArrayList<>();
+    private ArrayList<BoardElement> powerups = new ArrayList<>();
+    private ArrayList<BoardElement> drawQueue = new ArrayList<>();
+
+    public Board(String code) {
+        int u = 0;
+        for(int i = 0; i < 16; i++)
+            for(int j = 0; j < 18; j++) {
+                switch (code.charAt(i*18 + j)) {
+                    case '0':
+                        //TODO: Create blocks
+                        //blocks.add(new Block("#300000", new Position(j, i), false, this));
+                        u++;
+                        break;
+                    case '1':
+                        //blocks.add(new Block("#ffffff", new Position(j, i), true, this));
+                        u++;
+                        break;
+                }
+            }
+    }
+
+    public ArrayList<BoardElement> getBoard() {
+        return this.blocks;
+    }
+
+    public ArrayList<BoardElement> getDrawQueue() {
+        return drawQueue;
+    }
 }
