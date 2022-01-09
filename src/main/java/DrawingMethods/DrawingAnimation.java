b30fc35b75c51c5655a66f77af3036b8fb359001
André Ávila <avila_andre14@hotmail.com>
2022-01-09 15:02:15 +0000

    Update DrawingAnimation

--- src/main/java/DrawingMethods/DrawingAnimation.java
+++ src/main/java/DrawingMethods/DrawingAnimation.java
@@ -3,14 +3,29 @@ package DrawingMethods;
 import Structures.Position;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
+import java.util.ArrayList;
+
 public class DrawingAnimation {
     int frameIndex = 0;
     int currentFrameTick = 0;
     DrawingImage[] frames = null;
     int[] frameDuration = null;
 
-    DrawingAnimation(DrawingImage[] images_) {
+    DrawingAnimation(DrawingImage[] images_, int[] durations_) {
         this.frames = images_;
+        this.frameDuration = durations_;
+        if (!(frameDuration.length == frames.length))
+            if (frameDuration.length < frames.length) {
+                int[] newDurations = new int[frames.length];
+                for (int i = 0; i < frames.length; i++){
+                    if (i < frameDuration.length)
+                        newDurations[i] = frameDuration[i];
+                    else
+                        newDurations[i] = frameDuration[frameDuration.length-1];
+                }
+                frameDuration = newDurations;
+            }
+
     }
 
     public void draw(TextGraphics graphics_, Position pos_, boolean boardOffset_) {
