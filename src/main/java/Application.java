5a3d99c48e3ed9e1a3862e3398b7f85aa1a9c4a7
Malva2312 <up202006605@edu.fe.up.pt>
2022-01-17 22:40:25 +0000

    Update Application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -19,50 +19,13 @@ import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+
+
 public class Application {
     public static void main(String[] args) {
-        new Application().run();
-    }
-
-    private void run() {
         try {
-            Font font =  Font.createFont(Font.TRUETYPE_FONT,
-                    new File(getClass().getClassLoader().getResource("GameFont.ttf").toURI()));
-
-            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(font);
-
-            DefaultTerminalFactory factory = new DefaultTerminalFactory();
-
-            factory.setTerminalEmulatorFontConfiguration(
-                    AWTTerminalFontConfiguration.newInstance(
-                            font.deriveFont(Font.PLAIN, 12)));
-
-            factory.setForceAWTOverSwing(true);
-            factory.setInitialTerminalSize(new TerminalSize(98, 52));
-
-            Terminal terminal = factory.createTerminal();
-            ((AWTTerminalFrame)terminal).addWindowListener(new WindowAdapter() {
-                @Override
-                public void windowClosing(WindowEvent e) {
-                    e.getWindow().dispose();
-                }
-            });
-
-            ((AWTTerminalFrame) terminal).setTitle("BOMBERMAN");
-            ((AWTTerminalFrame) terminal).setIconImage(ImageIO.read(getClass().getClassLoader().getResource("icon.png")));
-            ((AWTTerminalFrame) terminal).setResizable(false);
-            
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-            screen.clear();
-            screen.refresh();
-
-            new MenuController(screen, 98, 52);
-
-            screen.stopScreen();
-
+            Manager manager = new Manager();
+            manager.start();
         } catch (IOException | InterruptedException | FontFormatException | URISyntaxException e) {
             e.printStackTrace();
         }
